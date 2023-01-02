4bd43e72928389c7bd31370422214299a8bbbc9a
GAY Federico David     [PROV] <C.FGAY@ternium.com.ar>
2023-01-02 20:07:35 -0300

    actualizamos navbar oculto

--- app.js
+++ app.js
@@ -4,9 +4,12 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, "public")));
 
-app.listen(3001, () => {
+/*app.listen(3001, () => {
     console.log("Conexion establecida con el servidor");
-});
+});*/
+
+const port = process.env.PORT || 3001;
+app.listen(port,()=>console.log('Servidor corriendo en puerto $(port)'));
 
 app.get("/", (req, res) => {
     res.sendFile(path.resolve(__dirname, "views/home.html"));
